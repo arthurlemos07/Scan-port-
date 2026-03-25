5a1773d3d9e34b6f9aaf8b8ddb522eb0c242e27f
arthurlemos07 <98622391+arthurlemos07@users.noreply.github.com>
2026-03-25 00:00:36 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package org.example;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Main {
+public class ScanPort {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
